91c4affcceefb88f0560513cf7e67d4924436151
jiaxiaoxiao <jiaxiaoxiao@zhexinit.com>
2019-12-02 18:02:33 +0800

    update

--- react-router-demo/src/pages/RouterPage/TestPage.jsx
+++ react-router-demo/src/pages/RouterPage/TestPage.jsx
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-
-class TestPage extends Component {
-  go =() =>{
-    const { history } = this.props
-    history.push('/search/111',{
-      state1:'11'
-    })
+import { withRouter } from 'react-router-dom';
+class Search extends Component {
+  go = () => {
+    const { history } = this.props;
+    history.push('/search/111', {
+      state1: '11',
+    });
+  };
+  render() {
+    return <button onClick={this.go}>去搜索页面</button>;
   }
+}
+const SearchChild = withRouter(Search);
+class TestPage extends Component {
   render() {
     const { match, location } = this.props;
-    console.log(match)
-    console.log(location)
+    console.log(match);
+    console.log(location);
     return (
       <div>
         <div>Test</div>
-        <button onClick={this.go}>去搜索页面</button>
+        <SearchChild />
       </div>
-    )
+    );
   }
 }
 
